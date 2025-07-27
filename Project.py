9e3eccd9e475c1eb4db3163532afd2c460215a2c
Jiya-Nair <jiyahnair2020@gmail.com>
2025-07-27 23:55:43 +0530

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,61 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import kagglehub
+import os
+
+# Download latest version
+path = kagglehub.dataset_download("mansoordaku/ckdisease")
+
+print("Path to dataset files:", path)
+csv_file= os.path.join(path,"chronic_kidney_disease.csv")
+df= pd.read_csv(csv_file)
+
+print("First five rows of the dataset:")
+print(df.head())
+
+print("\n dataset info:")
+print(df.info())
+
+print("\nMissing values in each column:")
+print(df.isnull().sum())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
